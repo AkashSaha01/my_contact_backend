17cab68ef0564b31eb81b7033daf5622697b37c3
AkashSaha01 <akashsaha661@gmail.com>
2025-03-15 20:38:32 +0600

    mongoose connection

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const errorHandler = require("./middleware/errorHandler")
+const errorHandler = require("./middleware/errorHandler");
+
+const connectDB = require("./config/dbConnection");
+connectDB();
 const app = express();
 const port = process.env.Port || 5000;
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use(errorHandler);
 app.listen(port, () => {
-    console.log(`This Server is running on the port:${port}`)
-})
+  console.log(`This Server is running on the port:${port}`);
+});
