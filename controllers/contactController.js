a8b53c3c58ec1d859e93a26231853066446f49d7
AkashSaha01 <akashsaha661@gmail.com>
2025-03-16 10:16:56 +0600

    update api

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -39,8 +39,20 @@ const createContact = asyncHandler(async (req, res) => {
 //@desc Update Contact
 //@desc PUT /api/contacts/:id
 //@access public
-const UpdateContact = asyncHandler(async (req, res) => {
-  res.status(201).json({ message: `Update contact for ${req.params.id}` });
+const updateContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+
+  const updatedContact = await Contact.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedContact);
 });
 
 //@desc Delete Contact
@@ -52,7 +64,7 @@ const deleteContact = asyncHandler(async (req, res) => {
 module.exports = {
   getContacts,
   getContact,
-  UpdateContact,
+  updateContact,
   createContact,
   deleteContact,
 };
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { getContacts, getContact, createContact, UpdateContact, deleteContact } = require("../controllers/contactController")
+const {
+  getContacts,
+  getContact,
+  createContact,
+  updateContact,
+  deleteContact,
+} = require("../controllers/contactController");
 router.route("/").get(getContacts).post(createContact);
-router.route("/:id").put(UpdateContact).get(getContact).delete(deleteContact);
+router.route("/:id").put(updateContact).get(getContact).delete(deleteContact);
 
 module.exports = router;
